51ca1d9ed243fe1811e7b7949fdd1ffe05ed415f
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-10-27 23:25:13 -0400

    set up application.js and application.html.erb to render react App component

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,11 @@
 require("@rails/ujs").start()
 require("channels")
 
+import React from 'react'
+import { render } from 'react-dom'
+
+import App from '../react/components/App'
+import RedBox from 'redbox-react'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -13,3 +18,20 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener('DOMContentLoaded', () => {
+  let reactElement = document.getElementById('app')
+
+  if (reactElement) {
+    if(window.railsEnv && window.railsEnv === 'development'){
+      try {
+        render(<App />, reactElement)
+      } catch (e) {
+        render(<RedBox error={e} />, reactElement)
+      }
+    }
+    else {
+      render(<App />, reactElement)
+    }
+  }
+})
