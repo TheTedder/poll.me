2a16175dfced86b746fe4ad849df4a017c61b3b9
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-10-28 16:41:37 -0400

    made test for polls index react component

---
+++ app/javascript/react/components/PollsIndex.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { mount, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import PollsIndex from './PollsIndex'
+
+describe('<PollsIndex />', () => {
+  let wrapper
+
+  beforeEach( () => {
+    wrapper = mount(
+      <PollsIndex />
+    )
+  })
+
+  it('should render a button with the text "Create a Poll"', () => {
+    expect(wrapper.find('a.button.new-poll-button')).toExist()
+  })
+})
