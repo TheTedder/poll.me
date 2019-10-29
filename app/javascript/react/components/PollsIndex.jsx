e8ddda82d509836d037054cd1304ed6a58287db1
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-10-28 22:47:29 -0400

    styling

--- app/javascript/react/components/PollsIndex.jsx
+++ app/javascript/react/components/PollsIndex.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PollsIndex = (props) => {
   return (
     <div className="text-center polls-index-wrapper">
-      <a className="big title button red new-poll-button" href="/new">Create New Poll</a>
+      <a className="big button alert new-poll-button" href="/new">Create New Poll</a>
     </div>
   )
 }
