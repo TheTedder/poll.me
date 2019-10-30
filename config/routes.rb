e799b62200677228b4ba55d81e96dfc709685095
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-10-30 17:11:35 -0400

    add show api route for polls

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :polls, only: [:create]
+      resources :polls, only: [:create, :show]
     end
   end
 end
