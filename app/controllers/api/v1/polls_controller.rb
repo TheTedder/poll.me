d12c0d12a2d8b8d99ec28d9cda37870041eadd6b
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-10-30 11:22:23 -0400

    made poll create route

--- app/controllers/api/v1/polls_controller.rb
+++ app/controllers/api/v1/polls_controller.rb
@@ -1,2 +1,17 @@
-class Api::V1::PollsController < ApplicationController
+class Api::V1::PollsController < 
+  def create
+    poll = Poll.new(poll_params)
+    params['poll']['options'].each do |text|
+      option = Option.new(name: text)
+      if option.save
+        poll.candidates << option
+      end
+    end
+    poll.save
+  end
+
+  private
+  def poll_params
+    params.require(:poll).permit(:name, :description)
+  end
 end
