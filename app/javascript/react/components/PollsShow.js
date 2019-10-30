19ff573ceb3446730d33c13d1f3faaabedaa4f2d
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-10-30 17:35:26 -0400

    made new page fetch from backend

--- app/javascript/react/components/PollsShow.js
+++ app/javascript/react/components/PollsShow.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const PollsShow = (props) => {
+  const [poll, setPoll] = useState(
+    {
+      id: null,
+      name: '',
+      description: '',
+      options: []
+    }
+  )
+
+  useEffect( () => {
+    fetch(`/api/v1/polls/${props.match.params.id}`, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then( (response) => {
+      if (response.ok){
+        return response
+      } else{
+        throw new Error(`${response.status} (${response.statusText})`)
+      }
+    })
+    .then( (response) => response.json() )
+    .then( (json) => {
+      debugger
+      setPoll(json.poll)
+    })
+  }, [])
+
+  
   return (
-    <div></div>
+    <div className="grid-padding-y">
+      <div className="grid-x grid-padding-x cell">
+        <div className="cell small-12 medium-9 medium-offset-1">
+          <div className="primary callout">
+            <h2 className="title">title</h2>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
