308576e3545bd2149eeb7338818504b9944618b0
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-10-30 11:10:32 -0400

    added poll create route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/new', to: 'homes#index'
   get '/', to: 'homes#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :polls, only: [:create]
+    end
+  end
 end
