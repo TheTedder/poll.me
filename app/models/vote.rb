096e4dadab64f9dc69957ffc23bc3a12b4ee34a0
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-10-31 15:24:57 -0400

    made it so votes must be for candidates in the actual poll

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,7 @@ class Vote < ApplicationModel
   belongs_to :candidate
   belongs_to :link
 
-  validates :candidate_id, presence: true
+  validates :candidate_id, inclusion: { in: link.poll.candidates  }
   validates :link_id, presence: true
   validates :link_id, uniqueness: true, if: -> { link.single_use }
 end
