d185e288216f5a76d8a6e9869611289c201e282c
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-10-31 16:06:09 -0400

    removed erroneous secure token length parameter

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,8 +1,8 @@
 class Link < ApplicationRecord
   belongs_to :poll
   has_many :votes
-  has_secure_token :slug, length: 6
-  
+  has_secure_token :slug
+
   validates :poll_id, presence: true
   validates :single_use, inclusion: { in: [true, false] }, allow_nil: true
 end
