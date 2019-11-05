33c287a8f351d895a9050a4c1a50e60abc93df73
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-11-05 18:16:20 -0500

    return errors from vote create route if any

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::VotesController < ApplicationController
   def create
     vote = Vote.new(link: Link.find_by(slug: params['token']), candidate_id: params['candidateId'])
     unless vote.save
-      head :bad_request
+      render json: vote.errors, status: :bad_request
     end
   end
 end
