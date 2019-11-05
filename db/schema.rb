33bae1a4e84ce2ab2e6a43667e8e83aab71edb67
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-11-05 18:31:04 -0500

    added voting deadline column in polls table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_194625) do
+ActiveRecord::Schema.define(version: 2019_11_05_232753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
