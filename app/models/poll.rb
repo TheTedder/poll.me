367b68a7437e1d88a9e9e6e5d60325b5c146932c
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-11-05 19:26:23 -0500

    added more validations for polls and votes

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -5,4 +5,16 @@ class Poll < ApplicationRecord
 
   validates :name, presence: true
   validates :candidates, length: { minimum: 2, message: 'must have at least two options' }, unless: -> { new_record? }
+  validate :valid_deadline
+
+  def open?
+    return (voting_deadline.nil? || voting_deadline >= DateTime.now) && links.any?{ |link| link.valid }
+  end
+
+  protected
+  def valid_deadline
+    unless voting_deadline.nil? || voting_deadline >= DateTime.now
+      errors.add(:voting_deadline, 'must not be in the past')
+    end
+  end
 end
