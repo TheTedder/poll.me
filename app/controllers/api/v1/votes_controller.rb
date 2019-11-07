2ae7fedcb50f991b3c69fc3b9c9db45eb8d38e2a
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-11-07 17:32:27 -0500

    update votes controller to handle rankings

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::VotesController < ApplicationController
   def create
-    vote = Vote.new(link: Link.find_by(slug: params['token']), candidate_id: params['candidateId'])
+    vote = Vote.create(link: Link.find_by(slug: params['token']))
+    params['vote']['candidates'].each.with_index(1) do |candidate_id, index|
+      Ranking.create(vote: vote, candidate_id: candidate_id, rank: index)
+    end
     unless vote.save
       render json: vote.errors, status: :bad_request
     end
