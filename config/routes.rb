94cdd64c6c70c9a12c552d502e2432f4f2635b6d
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2021-10-26 17:54:02 -0400

    add poll index api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :polls, only: [:create, :show] do
+      resources :polls, only: [:index, :create, :show] do
         resources :links, only: [:create]
       end
       resources :links, only: [:show]
